e9abb52e2628dd7f92736d036ec4a35bb7b15da1
KHK <ston0538@gmail.com>
2020-06-30 03:44:09 +0900

    [ADD] Infrun filter,reduce

---
+++ src/__tests__/infrun/filter.ts
@@ -0,0 +1,56 @@
+import _ from "lodash";
+describe("filter", () => {
+  const products = [
+    { name: "반팔티", price: 15000 },
+    { name: "긴팔티", price: 20000 },
+    { name: "가방", price: 30000 },
+    { name: "후드티", price: 25000 },
+    { name: "시계", price: 50000 },
+  ];
+  it("original filter ", () => {
+    let under20000 = [];
+    for (const p of products) {
+      if (p.price < 20000) under20000.push(p);
+    }
+    expect(under20000).toHaveLength(1);
+  });
+  it("custom filter ", () => {
+    const filter = (f, iter) => {
+      let result = [];
+      for (const i of iter) {
+        if (f(i)) result.push(i);
+      }
+      return result;
+    };
+    const result = filter((p) => p.price < 20000, products);
+    const result2 = filter(
+      (n) => n % 2,
+      (function* () {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+        yield 5;
+      })()
+    ); // 즉시실행함수
+    expect(result).toHaveLength(1);
+    expect(result2).toHaveLength(3);
+  });
+  it("lodash filter", () => {
+    const result = _.filter(products, (p) => p.price < 20000);
+    const result2 = _.filter(
+      (function* () {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+        yield 5;
+      })(),
+      (n) => n % 2
+    );
+    const result3 = _.filter(products, { price: 50000 });
+    expect(result).toHaveLength(1);
+    // expect(result2).toHaveLength(2);
+    expect(result3).toHaveLength(1);
+  });
+});
--- src/__tests__/infrun/reduce.ts
+++ src/__tests__/infrun/reduce.ts
@@ -1,10 +1,81 @@
+import _ from "lodash";
+const products = [
+  { name: "반팔티", price: 15000 },
+  { name: "긴팔티", price: 20000 },
+  { name: "가방", price: 30000 },
+  { name: "후드티", price: 25000 },
+  { name: "시계", price: 50000 },
+];
 describe("reduce1 배열안에 전부 더하기", () => {
-  it("loop", () => {
+  it("명령형", () => {
     const nums = [1, 2, 3, 4, 5];
     let total = 0;
     for (const n of nums) {
       total = total + n;
     }
     expect(total).toBe(15);
+  });
+
+  it("함수형", () => {
+    const reduce = (f, acc, iter) => {
+      if (!iter) {
+        iter = acc[Symbol.iterator]();
+        acc = iter.next().value;
+      }
+      for (const a of iter) {
+        acc = f(acc, a);
+      }
+      return acc;
+    };
+    const add = (a, b) => {
+      // console.log("a", a);
+      // console.log("b", b);
+      return a + b;
+    };
+    const totalPrice = (total_price, product) => {
+      // console.log(total_price);
+      // console.log(product);
+      return total_price + product.price;
+    };
+    // expect(add(add(add(add(add(0, 1), 2), 3), 4), 5)).toBe(15);
+
+    const result = reduce(add, [1, 2, 3, 4, 5]);
+    expect(result).toBe(15);
+
+    const result2 = reduce(
+      (total_price, product) => total_price + product.price,
+      0,
+      products
+    );
+    expect(result2).toBe(140000);
+
+    const result3 = reduce(totalPrice, 0, products);
+    expect(result3).toBe(140000);
+  });
+
+  it("함수형 acc가 없을때", () => {
+    const arr = [1, 2, 3, 4, 5];
+    const entries = arr[Symbol.iterator]();
+    const result = entries.next().value;
+    expect(result).toBe(1);
+  });
+  it("lodash", () => {
+    const add = (a, b) => a + b;
+    const totalPrice = (total_price, product) => {
+      // console.log(total_price);
+      // console.log(product);
+      return total_price + product.price;
+    };
+    const result = _.reduce([1, 2, 3, 4, 5], add);
+    expect(result).toBe(15);
+    const result2 = _.reduce(products, totalPrice, 0);
+    expect(result2).toBe(140000);
+  });
+
+  it("pure", () => {
+    const arr = [1, 2, 3, 4, 5];
+    const add = (a, b) => a + b;
+    const result = arr.reduce(add);
+    expect(result).toBe(15);
   });
 });
